cd83c9a4706bb1783dc47f2e79d81507ad8bcbde
Rehan Zafar <rehankhan983@gmail.com>
2022-03-12 15:12:04 +0530

    Update face_landmarks_MP.py

--- face_landmarks_MP.py
+++ face_landmarks_MP.py
@@ -2,19 +2,26 @@ import cv2
 import mediapipe as mp
 import time
 
-mp_drawing = mp.solutions.drawing_utils                 #inbuilt library for drawing over image
+#inbuilt library for drawing over image
+mp_drawing = mp.solutions.drawing_utils                 
 mp_drawing_styles = mp.solutions.drawing_styles
+ 
+# initializing face_mesh from mediapipe
 mp_face_mesh = mp.solutions.face_mesh
 
+# changing the 2 paramteres will change the points and lines drawn over image 
+# by default lines 64 to 89 are commented which uses these functions you can uncommet them and see the difference urself
 drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
 cap = cv2.VideoCapture('test.mp4')          #for a video file input
 # cap = cv2.VideoCapture(0)                 #for webcam input
 
+# reading height and width of your video/webcam input 
 success, image = cap.read()
 width = image.shape[1]
 height = image.shape[0]
 
 res = []
+
 
 fourcc = cv2.VideoWriter_fourcc(*"XVID")
 
@@ -22,14 +29,16 @@ fourcc = cv2.VideoWriter_fourcc(*"XVID")
 out = cv2.VideoWriter("output.avi", fourcc, 30, (width, height))
 
 
-# res[0].landmark.__hash__     
+# font type to write over image   
 font = cv2.FONT_HERSHEY_PLAIN
+
+# frames counter and start time to determine frame rate
 frames = 0
 start_time = time.time()
+
+# initializing with desired parameters(names are self explanatory)
 with mp_face_mesh.FaceMesh(
     max_num_faces=1,
-    
-    # refine_landmarks=True,
     min_detection_confidence=0.5,
     min_tracking_confidence=0.5) as face_mesh:
     try:
@@ -52,9 +61,8 @@ with mp_face_mesh.FaceMesh(
             image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
             if results.multi_face_landmarks:
                 for face_landmarks in results.multi_face_landmarks:
-                    
-                    res.append(face_landmarks)
-                    
+                   
+                    # each block of below codes have their own drawing abilities uncomment them one by one to check them out
                     
                     # mp_drawing.draw_landmarks(
                     #     image=image,
@@ -83,19 +91,32 @@ with mp_face_mesh.FaceMesh(
                     #     # .get_default_face_mesh_iris_connections_style())
                     #     )
                     
+                    
+                    # loop to draw circles at landmarks
                     for idd, lm in  enumerate(face_landmarks.landmark):
                         image = cv2.circle(image, (int(lm.x * width), int(lm.y * height)), 2, (255, 255, 0), 1)
                     
                         
                     
-                  
+            # writing FPS on each frame
             cv2.putText(image, 'FPS: ' + str(round(frames /(time.time() - start_time), 2)) , (10 , 40), font, fontScale = 2, color=(255, 55, 55), thickness=2)
+            
+            # writing frame to output video
             out.write(image)
+            
+            # showing frame on window
             cv2.imshow('Result Face Mesh', image)
+            
+            # on pressing esc key everything will quits
             key =  cv2.waitKey(1) 
             if key == 27:
               break
-          
+            
+        cap.release()
+        out.release()
+        cv2.destroyAllWindows()
+       
+    # if something unexpected happens everything quits
     except:
         cap.release()
         out.release()
